f3c5873e81db59c9acc8227c2368b9bf77530be8
s1771084 <s1771084@d-13102-092.ku-kinds.konan-u.ac.jp>
2018-10-31 13:53:33 +0900

    ガタガタ動きました

--- teamE/src/template/maze2D/teamE/MazeSpriteEnemyB.java
+++ teamE/src/template/maze2D/teamE/MazeSpriteEnemyB.java
@@ -1,5 +1,7 @@
 package template.maze2D.teamE;
 
+import framework.game2D.Map2D;
+import framework.game2D.Position2D;
 import framework.game2D.Sprite;
 
 
@@ -16,4 +18,31 @@ public class MazeSpriteEnemyB extends Sprite {
 	public MazeSpriteEnemyB(String imageFile) {
 		super(imageFile);
 	}
+
+
+	public void motion(long interval, Map2D mazeGround) {
+		Position2D gridPointEB = mazeGround.getNeighborGridPoint(mazeSpriteEnemyB);
+		setVelocity(0.0,4.5);
+		setPosition(gridPointEB);
+		if((int)(5*(Math.random()))==0){
+			setVelocity(0.0,0.0);
+		}
+		else if((int)(5*(Math.random()))==1) {
+			setVelocity(0,-4.5);
+		}
+		else if((int)(5*(Math.random()))==2) {
+			setVelocity(4.5,0);
+		}
+		else if((int)(5*(Math.random()))==3) {
+			setVelocity(0.0,4.5);
+		}
+
+		else if((int)(5*(Math.random()))==4) {
+			setVelocity(-4.5,0);
+		}
+
+
+		super.motion(interval, mazeGround);;
+	}
+
 }
