03250ff49b0e86873f696d593829199b3730c4be
s1771084 <s1771084@d-13102-092.ku-kinds.konan-u.ac.jp>
2018-11-09 14:40:58 +0900

    ランダムの動きが少し良くなりました。

--- teamE/src/template/maze2D/teamE/MazeSpriteEnemyB.java
+++ teamE/src/template/maze2D/teamE/MazeSpriteEnemyB.java
@@ -1,7 +1,4 @@
 package template.maze2D.teamE;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import framework.game2D.Map2D;
 import framework.game2D.Sprite;
 
@@ -14,62 +11,51 @@ import framework.game2D.Sprite;
  */
 public class MazeSpriteEnemyB extends Sprite {
 	private MazeSpriteEnemyB mazeSpriteEnemyB;
+	int time;
+	int Changetime=300;
+	double VY,VX;
 	public MazeSpriteEnemyB(String imageFile) {
 		super(imageFile);
 	}
 
-	public class Rando extends TimerTask{
-		public void run() {
-			System.out.println("hhh");
-			if((int)(5*(Math.random()))==0){
-				setVelocity(0.0,0.0);
-			}
-			else if((int)(5*(Math.random()))==1) {
-				setVelocity(0,-4.5);
-			}
-			else if((int)(5*(Math.random()))==2) {
-				setVelocity(4.5,0);
-			}
-			else if((int)(5*(Math.random()))==3) {
-				setVelocity(0.0,4.5);
-			}
 
-			else if((int)(5*(Math.random()))==4) {
-				setVelocity(-4.5,0);
-			}
-		}
-	}
-
-	//public class timer{
 	public void motion(long interval, Map2D mazeGround) {
+time+=interval;
+int rdm=(int) (5*(Math.random()));
 
+if(time>Changetime) {
+		if(rdm==0){
 
-
-		//Timer timer = new Timer();
-		//timer.schedule(new MazeSpriteEnemyB(), 1000,3000);
-
-		//setVelocity(0.0,4.5);
-		if((int)(5*(Math.random()))==0){
-			setVelocity(0.0,0.0);
+			VX=2.0;
+			VY=0.0;
 		}
-		else if((int)(5*(Math.random()))==1) {
-			setVelocity(0,-4.5);
+		else if(rdm==1) {
+
+			VX=0.0;
+			VY=-4.5;
 		}
-		else if((int)(5*(Math.random()))==2) {
-			setVelocity(4.5,0);
+		else if(rdm==2) {
+
+			VX=4.5;
+			VY=0.0;
 		}
-		else if((int)(5*(Math.random()))==3) {
-			setVelocity(0.0,4.5);
+		else if(rdm==3) {
+
+		VX=0.0;
+		VY=4.5;
+		}
+		else if(rdm==4) {
+
+			VX=-4.5;
+			VY=0.0;
 		}
 
-		else if((int)(5*(Math.random()))==4) {
-			setVelocity(-4.5,0);
-		}
-		Timer timer = new Timer();
-		timer.schedule(new Rando(), 0, 3000);
-		super.motion(interval, mazeGround);;
-
-
+		time=0;
+}
+setVelocity(VX,VY);
+//Timer timer = new Timer();
+//timer.schedule(new Rando(), 0, 3000);
+super.motion(interval, mazeGround);;
 	}
 
 }
